4cd1ba0cacc1bd630f27e3bf1d24d232005b9893
unknown <zumrudka@code.edu.az>
2020-04-28 15:27:26 +0400

    appointment modal

--- new/js/doctors.js
+++ new/js/doctors.js
@@ -1,6 +1,15 @@
 'use strict';
 $(function () {
-    
+    $(document).on('click', '.appoinment-form', function () {
+        $('.appointment-modal').show();
+
+        $(document).on('click', '.appointment-modal', function () {
+        $('.appointment-modal').hide();
+            
+        })
+    })
+
+
 
 
 
